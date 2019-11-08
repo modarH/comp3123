3a26c8fc7c926a3d180138d7abc77a5f411544d6
Modar Hamdan <modar.hamdan@georgebrown.ca>
2019-11-08 09:03:26 -0500

    lab7 completed

---
+++ lab7/angular8-meanstack-angular-material/backend/database/db.js
@@ -0,0 +1,3 @@
+  module.exports = {
+    db: 'mongodb://modar:admin@cluster0-shard-00-00-eutgt.mongodb.net:27017,cluster0-shard-00-01-eutgt.mongodb.net:27017,cluster0-shard-00-02-eutgt.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+  };
